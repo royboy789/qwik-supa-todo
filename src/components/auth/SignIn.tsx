afd98d19da2879af1507f344b6b8dda0d9076e0d
Roy Sivan <roy@roysivan.com>
2023-01-05 14:32:46 -0500

    removing test email

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,7 +1,7 @@
 import { component$, useStore, $ } from "@builder.io/qwik";
 
 const SignIn = component$(() => {
-  const user = useStore({ email: "roy@roysivan.com" });
+  const user = useStore({ email: "" });
   const initSignIn = $(async() => {
     // send magiclink
     await fetch(`/auth/login`, {
@@ -20,7 +20,7 @@ const SignIn = component$(() => {
             id="user_email"
             name="user_email"
             placeholder="Sign Up / Sign In with email"
-            value="roy@roysivan.com"
+            value=""
             onChange$={(e) =>
               (user.email = (e.target as HTMLInputElement).value)
             }
